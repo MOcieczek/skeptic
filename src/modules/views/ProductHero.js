ad428c1c269794a290322337948c2d7653822877
MOcieczek <monika.ocieczek@gmail.com>
2020-04-05 19:21:06 +0200

    cards

--- src/modules/views/ProductHero.js
+++ src/modules/views/ProductHero.js
@@ -9,7 +9,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import Placeholder from '../../assets/placeholder.png';
 import Logo from '../../assets/logo.jpg';
 
 
@@ -26,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '0%', // 16:9
   },
   cardContent: {
     flexGrow: 1,
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4, 5];
 
 export default function Album() {
   const classes = useStyles();
@@ -45,33 +45,87 @@ export default function Album() {
   return (
     <React.Fragment>
       <CssBaseline />
-      
-      
       <main>
         {/* Hero unit */}
         <div>
-          <Container maxWidth="sm" align="center">
+          <Container maxWidth="sm" align="center" paddingTop='20'>
             <img 
             src={Logo}
             width='400'
             alt="logo"
             ></img>
             <Typography variant="h5" align="center" color="textPrimary" paragraph>
-              I&apos;m Monika!
+              Fintech enthusiast and product freelancer
             </Typography>
-
           </Container>
         </div>
+        {/* End hero unit */}
+
         <Container className={classes.cardGrid} maxWidth="md">
-          {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+              <Grid item xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
+                  <CardMedia className={classes.cardMedia}/>
+                  <img 
+                  src={Placeholder}
+                  title="Gimi" 
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Gimi
+                    </Typography>
+                    <Typography>
+                      CPO and Co-Founder
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                  </CardActions>
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia className={classes.cardMedia}/>
+                  <img 
+                  src={Placeholder}
+                  title="Tink" 
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Tink
+                    </Typography>
+                    <Typography>
+                      Product Manager and Product team lead
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                  </CardActions>
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia className={classes.cardMedia}/>
+                  <img 
+                  src={Placeholder}
+                  title="Scalable" 
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Scalable.ai
+                    </Typography>
+                    <Typography>
+                      Product strategy and definition
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                  </CardActions>
+                </Card>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia className={classes.cardMedia}/>
+                  <img 
+                  src={Placeholder}
+                  title="Gimi" 
                   />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
@@ -82,16 +136,28 @@ export default function Album() {
                     </Typography>
                   </CardContent>
                   <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
                   </CardActions>
                 </Card>
               </Grid>
-            ))}
+              <Grid item xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia className={classes.cardMedia}/>
+                  <img 
+                  src={Placeholder}
+                  title="Gimi" 
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                  </CardActions>
+                </Card>
+              </Grid>
           </Grid>
         </Container>
       </main>
