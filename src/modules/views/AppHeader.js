aea6a396d84022b1aa2f982b9d88ca4330e8579b
MOcieczek <monika.ocieczek@gmail.com>
2020-04-05 21:49:53 +0200

    Summary

--- src/modules/views/AppHeader.js
+++ src/modules/views/AppHeader.js
@@ -3,6 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 import Logo from '../../assets/logo.jpg';
 
 
@@ -10,10 +11,13 @@ const useStyles = makeStyles((theme) => ({
 
 
   container: {
-    paddingTop: theme.spacing(8),
+    paddingTop: theme.spacing(20),
     paddingBottom: theme.spacing(8),
   },
-
+  link: {
+    variant: "h5",
+    color: "blue",
+  },
 
 }));
 
@@ -25,14 +29,21 @@ export default function Album() {
     <React.Fragment>
       <CssBaseline />
       <main>
-        <Container maxWidth="sm" align="center" paddingTop='20'>
+        <Container maxWidth="sm" align="center" justifyContent="center" paddingTop="20" className={classes.container}>
             <img 
             src={Logo}
             width='400'
             alt="logo"
             ></img>
             <Typography variant="h5" align="center" color="textPrimary" paragraph>
-              Fintech enthusiast and product freelancer
+            Hi, I'm Monika! 
+            <br></br>
+            I work as a product owner/manager/designer, mostly, but not exclusively, with fintech. With experience from <Link className={classes.link} target="_blank" to='http://www.gimitheapp.com'>Gimi</Link>, <Link className={classes.link} target="_blank" to='http://www.tink.com'>Tink</Link>, <Link className={classes.link} target="_blank" to='http://www.scalable.company'>Scalable</Link>, 
+              , I know how to make great products come to life. 
+            </Typography>
+            <br></br>
+            <Typography fontSize="6" color="textSecondary">
+              monika.ocieczek@gmail.com
             </Typography>
           </Container>
       </main>
