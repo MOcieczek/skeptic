4c2bd581727f7f6f3c2f3728f491ef4ef7cba762
MOcieczek <monika.ocieczek@gmail.com>
2020-04-05 21:00:49 +0200

    Removed appFooter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import withRoot from './modules/withRoot'
 import AppHeader from './modules/views/AppHeader';
-import AppFooter from './modules/views/AppFooter.js';
 import './App.css';
 
 function Index() {
   return (
     <React.Fragment>  
       <AppHeader/>
-      <AppFooter />
     </React.Fragment>
   )
 }
