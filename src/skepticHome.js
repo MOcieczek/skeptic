d3f5a6eb6dbdfd0ae2bebd8f53d1ad06da771862
MOcieczek <monika.ocieczek@gmail.com>
2020-04-06 16:35:50 +0200

    Fixed black background for skepticHome

--- src/skepticHome.js
+++ src/skepticHome.js
@@ -4,10 +4,16 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Link } from 'react-router-dom';
 import Logo from './assets/logo.jpg';
-  
 
 const useStyles = makeStyles((theme) => ({
-
+  
+  root: {
+    display: 'flex',
+    overflow: 'hidden',
+    backgroundColor: "rgb(0,0,0)",
+    backgroundSize: "cover",
+    bottom: 0,
+    },
 
   container: {
     paddingTop: theme.spacing(10),
@@ -19,10 +25,11 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     position: "relative",
     maxWidth: "sm",
+    color: "#ffffff",
   },
   links: {
     variant: "h5",
-    color: "blue",
+    color: "#FFD700",
     textDecoration: 'none',
   },
   image: {
@@ -31,7 +38,11 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(5),
   },
   body2: {
-    color: "rgb(0,0,0,0.4)",
+    variant: "body2",
+    color: "rgb(255,255,255,0.8)",
+  },
+  body2: {
+    color: "rgb(255,255,255)",
     textDecoration: 'none',
   },
 }));
@@ -41,13 +52,14 @@ export default function AppHeader() {
   const classes = useStyles();
 
   return (
+    <section className={classes.root}>
         <Container maxWidth="sm" className={classes.container}>
             <img 
             src={Logo}
             className={classes.image}
             alt="logo"
             ></img>
-            <Typography variant="h5" align="center" color="textPrimary" paragraph>
+            <Typography variant="h5" align="center" color="#ffffff" paragraph>
             This is Skeptic <span role="img" aria-label="hi">🙌</span>
             <br></br>
             <br></br>
@@ -57,19 +69,26 @@ export default function AppHeader() {
             </Typography>
             <br></br>
             <div>
-            <Typography variant="h5" align="center" color="textPrimary" paragraph>
+            <Typography variant="h5" align="center" paragraph>
               When I'm not Skeptic, I'm <Link className={classes.links} to="/monika">Monika</Link>
             </Typography>
             </div>
             <br></br>
             <div>
-            <Typography variant="body2" className={classes.body2} align="center" paragraph>
-            <span role="img" aria-label="star">✨</span> <a className={classes.link} href="mailto:monika.ocieczek@gmail.com">
+            <Typography className={classes.body2} align="center" paragraph>
+            <span role="img" aria-label="star">✨</span> <a className={classes.links} href="mailto:monika.ocieczek@gmail.com">
                Get in touch!
               </a> 
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
             </Typography>  
             </div>
           </Container>
-      
+
+      </section>
   );
 }
