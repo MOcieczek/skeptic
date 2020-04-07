2fdbe39fe49540acb9e4478e8823d31e5bc5fb24
MOcieczek <monika.ocieczek@gmail.com>
2020-04-07 11:02:40 +0200

    removed header links

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import React, { Component } from "../node_modules/react";
 import Route from "react-router-dom/Route";
-import NavLink from "react-router-dom/NavLink";
 import Hashrouter from "react-router-dom/HashRouter";
 import Skeptic from "./skepticHome";
 import SkepticMonika from "./skepticMonika";
@@ -9,15 +8,8 @@ class Main extends Component {
   render() {
     return (
       <Hashrouter>
-        <div>
-          <ul className="header">
-            <NavLink exact to="/">Skeptic</NavLink> or <NavLink to="/monika">Monika</NavLink>?
-          </ul>
-          <div>
             <Route exact path="/" component={Skeptic}/>
             <Route path="/monika" component={SkepticMonika}/>
-          </div>
-        </div>
         </Hashrouter>
     );
   }
